9736072f88738c9952f1e3ca09ad6a3b50a42856
werthmannsupercode <werthmannsupercode@gmail.com>
2022-08-04 16:39:04 +0200

    deploy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 export const apiBaseUrl = process.env.REACT_APP_API_PATH === "dev"
     ? "http://localhost:9000"
-    : "https://sc-kino-backend.herokuapp.com"
+    : "https://kontaktformular-backend.herokuapp.com/"
 
         // (Link der deployten Heroku)
