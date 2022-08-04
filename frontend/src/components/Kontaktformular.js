e1609ffbae5f2a19edb47bc41e2151ce2e6243b9
werthmannsupercode <werthmannsupercode@gmail.com>
2022-08-04 16:59:43 +0200

    another

--- frontend/src/components/Kontaktformular.js
+++ frontend/src/components/Kontaktformular.js
@@ -15,14 +15,14 @@ const Kontaktformular = () => {
     const [show, setShow] = useState(false)
     const [checkbox, setCheckbox] = useState(false)
 
-    // function handleErrors(response) {
-    //     if (!response.ok) {
-    //         return alert("Bitte überprüfe deine Eingabe, deine Nachricht konnte nicht übermittelt werden.")
-    //     }
-    //     else {
-    //         return setShow(true), setName(""), setText(""), setEmail(""), setCheckbox(false);
-    //     }
-    // }
+    function handleErrors(response) {
+        if (!response.ok) {
+            return alert("Bitte überprüfe deine Eingabe, deine Nachricht konnte nicht übermittelt werden.")
+        }
+        else {
+            return setShow(true), setName(""), setText(""), setEmail(""), setCheckbox(false);
+        }
+    }
 
     const sendNewRequest = (e) => {
         e.preventDefault();
@@ -53,7 +53,8 @@ const Kontaktformular = () => {
                     email,
                     text
                 })
-            }).then(res => res.json(), setShow(true), setName(""), setText(""), setEmail(""), setCheckbox(false))
+            }).then(handleErrors)
+                .then(res => res.json())
                 .catch((error) => {
                     console.log(error)
                 })
